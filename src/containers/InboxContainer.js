e604282dcf825bcbd773d664dcede352bfb7ced4
Agha Ali Kazim <kazim@focusteck.com>
2020-09-30 15:47:33 +0500

    new changes

--- src/containers/InboxContainer.js
+++ src/containers/InboxContainer.js
@@ -8,18 +8,20 @@ import React, { Component } from 'react';
 import {
   View,
   Text,
-  StyleSheet,
+  StyleSheet, ScrollView
 } from 'react-native';
 import { Body, Container, H1, H3, Header, Left, Right, Title } from 'native-base';
 import SegmentedControlTab from 'react-native-segmented-control-tab'
 import NoMessages from '../screen/InboxScreen/NoMessages';
 import NotificationNot from '../screen/InboxScreen/NotifcationNot';
+import { GiftedChat } from 'react-native-gifted-chat';
 
 export default class InboxContainer extends Component {
   constructor(props){
     super(props)
     this.state = {
       showMessages: true,
+      messages:[],
       segmentTab:["Messages", "Notifications"],
       selectedIndex:0
     }
@@ -33,40 +35,9 @@ handleIndexChange = (values) =>{
 
   render() {
     return (
-      <Container>
-        <Header transparent>
-          <Left>
-            <H3>Inbox</H3>
-          </Left>
-          <Body />
-          <Right />
-        </Header>
-        <SegmentedControlTab
-          borderRadius={0}
-          tabsContainerStyle={{ height: 50, backgroundColor: '#F2F2F2',marginLeft:10, marginRight:10 }}
-          tabStyle={{ backgroundColor: 'white',fontSize:16, borderWidth: 0, borderColor: 'transparent', alignItems:'baseline' }}
-          activeTabStyle={{ backgroundColor: 'white',borderBottomColor:'green', marginBottom: 2, borderBottomWidth:2, textAlign:'left' }}
-          tabTextStyle={{ color: 'black', }}
-          activeTabTextStyle={{ color: 'black' }}
-          values={this.state.segmentTab}
-          selectedIndex={this.state.selectedIndex}
-          onTabPress={this.handleIndexChange}
-        />
-        {
-          this.state.showMessages
-          ? 
-          <NoMessages />
-          :
-          null
-        }
-        {
-          this.state.showNotifications
-          ?
-          <NotificationNot />
-          :
-          null
-        }
-      </Container>
+      <GiftedChat
+        messages={this.state.messages}
+      />
     );
   }
 }
